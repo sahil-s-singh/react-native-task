7af8d26c9386d2a95627df3039ad4a4fbd89f006
sahil-s-singh <68877454+sahil-s-singh@users.noreply.github.com>
2024-06-22 12:57:54 -0600

    remove console

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -3,16 +3,13 @@ import { Product } from '../types/product';
 
 export const fetchProducts = async (): Promise<Product[]> => {
   try {
-    const response = await axios.get('https://dummyjson.com/products');
-
-console.log(response, 'response');
-    
+    const response = await axios.get('https://dummyjson.com/products');    
     const products = response.data.products.map((product: any) => ({
       id: product.id,
       name: product.title,
       price: product.price,
       description: product.description,
-      image: 'product.thumbnail',
+      image: product.thumbnail,
     }));
     return products;
   } catch (error) {
